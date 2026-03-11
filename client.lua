dd302554e8f80e09ecfae54e1d3dba6eed0b3b57
ZUNII <50987498+ZUNII@users.noreply.github.com>
2026-03-11 11:07:02 +0100

    client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,5 @@
 -- client.lua
 
-guiSetInputMode("no_binds_when_editing")
-
 local mainWin, resGrid, fileGrid
 local btnNew, btnCopy, btnRename, btnDelete, btnCloseMain
 local editWin, editMemo, btnSave, btnRevert, btnCloseEdit, tplCombo, tplBtn
@@ -11,43 +9,160 @@ local currentRes, currentFile, originalContent = nil, nil, ""
 local promptAction = "" 
 local resListCache = {}
 
-local TEMPLATES = {
-    ["Jump Marker"] = [[
--- Jump Marker
-local jumpMarker1 = createMarker(X, Y, Z, "corona", 5, 255, 255, 255, 255)
+-- ==========================================
+-- SMART INPUT MANAGER (Fixes hotkey bleeding)
+-- ==========================================
+local function updateInput()
+    if guiGetVisible(editWin) or guiGetVisible(promptWin) then
+        guiSetInputEnabled(true) -- Hard captures keyboard, blocking 'P', 'T', etc.
+    else
+        guiSetInputEnabled(false) -- Releases keyboard back to the game
+    end
+end
 
-function onJumpMarkerHit1(player)    
+-- ==========================================
+-- AUTO-INCREMENT PARSER
+-- ==========================================
+local function getNextIndex(prefix)
+    local text = guiGetText(editMemo)
+    local maxIdx = 0
+    for match in string.gmatch(text, prefix .. "(%d+)") do
+        local num = tonumber(match)
+        if num and num > maxIdx then
+            maxIdx = num
+        end
+    end
+    return maxIdx + 1
+end
+
+-- ==========================================
+-- DYNAMIC TEMPLATE GENERATOR
+-- ==========================================
+local function generateTemplate(templateName)
+    if templateName == "Jump Marker" then
+        local idx = getNextIndex("jumpMarker")
+        return string.format([[
+
+-- Jump Marker %d
+local jumpMarker%d = createMarker(0, 0, 5, "corona", 5, 255, 255, 255, 255)
+
+function onJumpMarkerHit%d(player)    
     if player == localPlayer and isPedInVehicle(player) then
         local vehicle = getPedOccupiedVehicle(player)   
         if vehicle then 
-            setElementVelocity(vehicle, velX, velY, velZ)     
+            setElementVelocity(vehicle, 0, 0, 0.5)     
         end
     end
 end
-addEventHandler("onClientMarkerHit", jumpMarker1, onJumpMarkerHit1)
+addEventHandler("onClientMarkerHit", jumpMarker%d, onJumpMarkerHit%d)
+]], idx, idx, idx, idx, idx)
 
-]],
-    ["Teleport Marker"] = [[
--- Teleport Marker
-local tpMarker1 = createMarker(startX, startY, startZ, "corona", 5, 255, 255, 255, 255)
+    elseif templateName == "Teleport Marker" then
+        local idx = getNextIndex("tpMarker")
+        return string.format([[
 
-function onTpMarkerHit1(player)    
+-- Teleport Marker %d
+local tpMarker%d = createMarker(0, 0, 5, "corona", 5, 255, 255, 255, 255)
+
+function onTpMarkerHit%d(player)    
     if player == localPlayer and isPedInVehicle(player) then
         local vehicle = getPedOccupiedVehicle(player)   
         if vehicle then 
-            setElementPosition(vehicle, destX, destY, destZ)     
+            setElementPosition(vehicle, 10, 10, 5) -- Change destination here     
         end
     end
 end
-addEventHandler("onClientMarkerHit", tpMarker1, onTpMarkerHit1)
+addEventHandler("onClientMarkerHit", tpMarker%d, onTpMarkerHit%d)
+]], idx, idx, idx, idx, idx)
 
-]]
-}
+    elseif templateName == "Explosion Rotation Marker" then
+        local idx = getNextIndex("expRotMarker")
+        return string.format([[
 
+-- Explosion Rotation Marker %d
+local expRotMarker%d = createMarker(0, 0, 5, "corona", 5, 255, 0, 0, 255)
+
+function onExpRotMarkerHit%d(player)    
+    if player == localPlayer and isPedInVehicle(player) then
+        local vehicle = getPedOccupiedVehicle(player)   
+        if vehicle then 
+            local targetRotZ = 90 -- The required Z rotation
+            local tolerance = 30 -- Degrees of tolerance
+
+            local _, _, rz = getElementRotation(vehicle)
+            local diff = math.abs(rz - targetRotZ)
+            if diff > 180 then diff = 360 - diff end
+
+            if diff > tolerance then
+                local x, y, z = getElementPosition(vehicle)
+                createExplosion(x, y, z, 7) -- Visual explosion
+                setElementHealth(vehicle, 0) -- Destroy vehicle
+            end
+        end
+    end
+end
+addEventHandler("onClientMarkerHit", expRotMarker%d, onExpRotMarkerHit%d)
+]], idx, idx, idx, idx, idx)
+
+    elseif templateName == "Bounce Marker" then
+        local idx = getNextIndex("bounceMarker")
+        return string.format([[
+
+-- Bounce Marker %d
+local bounceMarker%d = createMarker(0, 0, 5, "corona", 5, 255, 255, 0, 255)
+
+function onBounceMarkerHit%d(player)    
+    if player == localPlayer and isPedInVehicle(player) then
+        local vehicle = getPedOccupiedVehicle(player)   
+        if vehicle then 
+            local vx, vy, vz = getElementVelocity(vehicle)
+            local bounceMultiplier = -1.2 -- Negative to go backwards, >1 to gain speed
+            
+            -- Keep vertical momentum normal, but flip and boost horizontal
+            setElementVelocity(vehicle, vx * bounceMultiplier, vy * bounceMultiplier, vz)     
+        end
+    end
+end
+addEventHandler("onClientMarkerHit", bounceMarker%d, onBounceMarkerHit%d)
+]], idx, idx, idx, idx, idx)
+
+    elseif templateName == "Rotation Modifier Marker" then
+        local idx = getNextIndex("rotModMarker")
+        return string.format([[
+
+-- Rotation Modifier Marker %d
+local rotModMarker%d = createMarker(0, 0, 5, "corona", 5, 0, 255, 255, 255)
+
+function onRotModMarkerHit%d(player)    
+    if player == localPlayer and isPedInVehicle(player) then
+        local vehicle = getPedOccupiedVehicle(player)   
+        if vehicle then 
+            local targetRX, targetRY, targetRZ = 0, 0, 180
+            
+            -- Save current velocity
+            local vx, vy, vz = getElementVelocity(vehicle)
+            
+            -- Apply new rotation
+            setElementRotation(vehicle, targetRX, targetRY, targetRZ)
+            
+            -- Restore velocity so momentum isn't lost
+            setElementVelocity(vehicle, vx, vy, vz)
+        end
+    end
+end
+addEventHandler("onClientMarkerHit", rotModMarker%d, onRotModMarkerHit%d)
+]], idx, idx, idx, idx, idx)
+    end
+
+    return ""
+end
+
+-- ==========================================
+-- GUI CREATION
+-- ==========================================
 local function buildGUI()
     local sw, sh = guiGetScreenSize()
     
-    -- MAIN WINDOW (Rebranded)
     mainWin = guiCreateWindow(sw/2 - 350, sh/2 - 250, 700, 500, "Dynamic Resource Editor", false)
     guiWindowSetSizable(mainWin, false)
     
@@ -63,7 +178,6 @@ local function buildGUI()
     btnCloseMain = guiCreateButton(600, 460, 90, 30, "Close", false, mainWin)
     guiSetVisible(mainWin, false)
 
-    -- EDITOR WINDOW
     editWin = guiCreateWindow(sw/2 - 400, sh/2 - 325, 800, 650, "Editor", false)
     guiWindowSetSizable(editWin, true)
     
@@ -72,16 +186,17 @@ local function buildGUI()
     btnSave = guiCreateButton(10, 540, 120, 30, "Save & Refresh", false, editWin)
     btnRevert = guiCreateButton(140, 540, 100, 30, "Revert", false, editWin)
     
-    tplCombo = guiCreateComboBox(260, 540, 150, 100, "Select Template...", false, editWin)
+    tplCombo = guiCreateComboBox(260, 540, 200, 150, "Select Template...", false, editWin)
     guiComboBoxAddItem(tplCombo, "Jump Marker")
     guiComboBoxAddItem(tplCombo, "Teleport Marker")
-    tplBtn = guiCreateButton(420, 540, 100, 30, "Insert", false, editWin)
+    guiComboBoxAddItem(tplCombo, "Explosion Rotation Marker")
+    guiComboBoxAddItem(tplCombo, "BW Marker")
+    guiComboBoxAddItem(tplCombo, "Rotation Modifier Marker")
+    tplBtn = guiCreateButton(470, 540, 100, 30, "Insert", false, editWin)
     
-    -- Button Rebranded
     btnCloseEdit = guiCreateButton(690, 540, 100, 30, "Close Editor", false, editWin)
     guiSetVisible(editWin, false)
 
-    -- ACTION PROMPT WINDOW
     promptWin = guiCreateWindow(sw/2 - 150, sh/2 - 100, 300, 200, "Action", false)
     guiWindowSetSizable(promptWin, false)
     promptLabel = guiCreateLabel(10, 30, 280, 20, "Enter details:", false, promptWin)
@@ -91,7 +206,6 @@ local function buildGUI()
     btnPromptCancel = guiCreateButton(190, 160, 100, 30, "Cancel", false, promptWin)
     guiSetVisible(promptWin, false)
 
-    -- Double Click Editor Launch
     addEventHandler("onClientGUIDoubleClick", fileGrid, function()
         local row = guiGridListGetSelectedItem(fileGrid)
         if row ~= -1 and currentRes then
@@ -100,7 +214,6 @@ local function buildGUI()
         end
     end, false)
 
-    -- Primary Click Handlers
     addEventHandler("onClientGUIClick", root, function(btn)
         if btn ~= "left" then return end
         
@@ -121,15 +234,17 @@ local function buildGUI()
             local sel = guiComboBoxGetSelected(tplCombo)
             if sel ~= -1 then
                 local txt = guiComboBoxGetItemText(tplCombo, sel)
-                if TEMPLATES[txt] then 
+                local dynamicCode = generateTemplate(txt)
+                if dynamicCode ~= "" then 
                     local currentCode = guiGetText(editMemo)
-                    guiSetText(editMemo, currentCode .. TEMPLATES[txt]) 
+                    guiSetText(editMemo, currentCode .. dynamicCode) 
                 end
             end
 
         elseif source == btnCloseEdit then
             guiSetVisible(editWin, false)
             showCursor(guiGetVisible(mainWin))
+            updateInput()
 
         elseif source == btnDelete then
             local row = guiGridListGetSelectedItem(fileGrid)
@@ -147,6 +262,7 @@ local function buildGUI()
             guiSetVisible(promptCombo, false)
             guiSetVisible(promptWin, true)
             guiBringToFront(promptWin)
+            updateInput()
 
         elseif source == btnRename then
             local row = guiGridListGetSelectedItem(fileGrid)
@@ -159,6 +275,7 @@ local function buildGUI()
             guiSetVisible(promptCombo, false)
             guiSetVisible(promptWin, true)
             guiBringToFront(promptWin)
+            updateInput()
 
         elseif source == btnCopy then
             local row = guiGridListGetSelectedItem(fileGrid)
@@ -174,9 +291,12 @@ local function buildGUI()
             guiSetVisible(promptCombo, true)
             guiSetVisible(promptWin, true)
             guiBringToFront(promptWin)
+            updateInput()
 
         elseif source == btnPromptCancel then
             guiSetVisible(promptWin, false)
+            updateInput()
+            
         elseif source == btnPromptOK then
             local txt = guiGetText(promptEdit)
             if txt == "" then return end
@@ -192,11 +312,14 @@ local function buildGUI()
                 triggerServerEvent("editor:copyFile", localPlayer, currentRes, currentFile, targetRes, txt)
             end
             guiSetVisible(promptWin, false)
+            updateInput()
 
         elseif source == btnCloseMain then
             guiSetVisible(mainWin, false)
             guiSetVisible(promptWin, false)
+            guiSetVisible(editWin, false)
             showCursor(false)
+            updateInput()
         end
     end)
 end
@@ -206,7 +329,16 @@ addEventHandler("onClientResourceStart", resourceRoot, buildGUI)
 bindKey("F2", "down", function()
     local state = not guiGetVisible(mainWin)
     guiSetVisible(mainWin, state)
+    
+    if not state then
+        -- Close sub-windows safely if hiding everything
+        guiSetVisible(editWin, false)
+        guiSetVisible(promptWin, false)
+    end
+    
     showCursor(state)
+    updateInput()
+    
     if state then triggerServerEvent("editor:requestResources", localPlayer) end
 end)
 
@@ -234,10 +366,10 @@ addEvent("editor:receiveContent", true)
 addEventHandler("editor:receiveContent", root, function(content, resName, fName)
     originalContent = content
     guiSetText(editMemo, content)
-    -- Window Text Rebranded
     guiSetText(editWin, "Editing: " .. resName .. " -> " .. fName)
     guiSetVisible(editWin, true)
     guiBringToFront(editWin)
+    updateInput()
 end)
 
 addEvent("editor:syncDirectory", true)
